d4552266274b4830c0a895edaf23b592e51fc402
Yash Mishra <ym911837@student.nitw.ac.in>
2023-07-11 15:10:30 +0530

    first commit

---
+++ users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUSerChangeForm
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm,
+    form = CustomUSerChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'age', 'is_staff', ]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
